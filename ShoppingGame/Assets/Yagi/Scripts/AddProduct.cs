00ce4e43054ce4771ac289a126fad82b4211fffb
yagikun <yagikundayo@gmail.com>
2020-08-22 21:49:57 +0900

    リスト作成画面を作成

---
+++ ShoppingGame/Assets/Yagi/Scripts/AddProduct.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*「+ 商品を追加」ボタンを押した時*/
+
+public class AddProduct : MonoBehaviour
+{
+    int ContentsNum;        //コンテンツの個数を格納
+    [SerializeField] GameObject ListContainer;      //リストをPrefabから持ってくる
+    
+    [SerializeField] GameObject Containers; //コンテンツを入れて置くゲームオブジェクト
+    [SerializeField] GameObject Area;       //スクロールするエリア
+    RectTransform AreaRect;                 //エリアのRectTransform
+
+    [SerializeField] ListName listname;          //商品を管理するスクリプト
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContentsNum = 1;
+        AreaRect = Area.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //ボタンを押した時
+    public void AddProductPush()
+    {        
+        //新しいリストを追加
+        listname.ListContainerEntity.Add(Instantiate(ListContainer, new Vector3(Containers.transform.position.x, Containers.transform.position.y - (listname.ListLen)*200, Containers.transform.position.z), Quaternion.identity, Containers.transform));
+        
+        //コンテナの位置を変更
+        Containers.transform.localPosition = new Vector3(Containers.transform.localPosition.x, Containers.transform.localPosition.y + 100, Containers.transform.localPosition.z);
+        //エリアのサイズを変更
+        AreaRect.sizeDelta = new Vector2(AreaRect.sizeDelta.x, AreaRect.sizeDelta.y+200);
+        //ボタンは最下部に置く
+        this.transform.localPosition = new Vector3(0, -AreaRect.sizeDelta.y/2+100, 0);
+        //IDを与える
+        listname.ListContainerEntity[listname.ListLen].GetComponent<ProductContainerScript>().ListID = listname.ListLen;
+        listname.ListLen++;             //商品数を増やす
+    }
+}
---
+++ ShoppingGame/Assets/Yagi/Scripts/ProductContainerScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*リスト1つを管理するスクリプト*/
+
+public class ProductContainerScript : MonoBehaviour
+{
+    GameObject listnameObject;      //商品名を管理するスクリプトを含むオブジェクト
+    ListName listname;         //商品名を管理するスクリプト
+
+    public int ListID;      //この商品のID
+
+    GameObject Containor;      //リストを格納するエリア
+    GameObject ScrollArea;      //スクロールエリア
+    RectTransform AreaRect;     //スクロールエリアのサイズ
+
+    GameObject AddButton;       //追加ボタン
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listnameObject = GameObject.Find("ViewArea");
+        listname = listnameObject.GetComponent<ListName>();
+
+        Containor = GameObject.Find("Containers");
+        ScrollArea = GameObject.Find("ScrollArea");
+        AreaRect = ScrollArea.GetComponent<RectTransform>();
+
+        AddButton = GameObject.Find("AddButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //削除ボタンを押した時
+    public void DeleteButton()
+    {
+        listname.ListLen--;
+
+        //一番下が削除されるため、内容を入れ替えてあげる必要がある
+        for(int i = ListID; i < listname.ListLen; i++)
+        {
+            listname.ListContainerEntity[i].transform.GetChild(1).GetComponent<InputField>().text = listname.ListContainerEntity[i+1].transform.GetChild(1).GetComponent<InputField>().text;
+        }
+
+        //一番下のオブジェクトを削除
+        Destroy(listname.ListContainerEntity[listname.ListLen].gameObject);
+        listname.ListContainerEntity.RemoveAt(listname.ListLen);
+
+        //コンテナの位置を変更
+        Containor.transform.localPosition = new Vector3(Containor.transform.localPosition.x, Containor.transform.localPosition.y - 100, Containor.transform.localPosition.z);
+        //エリアのサイズを変更
+        AreaRect.sizeDelta = new Vector2(AreaRect.sizeDelta.x, AreaRect.sizeDelta.y - 200);
+        //追加ボタンの位置を上げる
+        AddButton.transform.localPosition = new Vector3(0, -AreaRect.sizeDelta.y / 2 + 100, 0);
+    }
+}
