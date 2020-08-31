f223761266af7a03cb61f9aa251fe5b68d32b81c
yagikun <yagikundayo@gmail.com>
2020-08-31 15:43:17 +0900

    IDをかぶりの内容に自動生成するプログラム完成

--- ShoppingGame/Assets/Yagi/Scripts/_AnyScene/IDCreate.cs
+++ ShoppingGame/Assets/Yagi/Scripts/_AnyScene/IDCreate.cs
@@ -11,9 +11,11 @@ public class IDCreate : MonoBehaviour
     int ID_int;         //int型のID
     string ID_str = "";      //string型のID
 
-    [SerializeField] Text ID_text;      //自分のIDヲ表示するテキスト
+    [SerializeField]
+    Text ID_text;      //自分のIDヲ表示するテキスト
 
-    [SerializeField] bool IDResetF;
+    [SerializeField]
+    bool IDResetF;
 
     // Start is called before the first frame update
     void Start()
@@ -39,84 +41,79 @@ public class IDCreate : MonoBehaviour
         {
             //IDが既に存在する場合、ID_strに渡しておく
             ID_str = PlayerPrefs.GetString("IDCreateYet", "null");
+            //IDを表示する
+            ID_text.text = "ID:" + ID_str;
         }
-        
-        //IDを表示する
-        ID_text.text = "ID:" + ID_str;
 
 
 
 
 
 
-        
+        Debug.Log("Start終了");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     //IDを作成する関数
     void CreateID()
     {
-        bool IDCreateF = false;     //IDが被っているかどうかのフラグ
-
-        do
+        ID_int = (int)Random.Range(0, 999);       //0～999の乱数を生成し、IDにする
+                                                 //IDをstring型に直す
+        if (ID_int < 100)
         {
-            Debug.Log("実行されました");
-            ID_int = (int)Random.Range(0, 10);       //0～999の乱数を生成し、IDにする
-                                                     //IDをstring型に直す
-            if (ID_int < 100)
+            ID_str += "0";
+            if (ID_int < 10)
             {
                 ID_str += "0";
-                if (ID_int < 10)
-                {
-                    ID_str += "0";
-                }
             }
-            ID_str += ID_int.ToString();
+        }
+        ID_str += ID_int.ToString();
+        
 
-            IDCreateF = false;
-            Debug.Log("ここまでできた");
-            //取得テスト
-            //UserIDsを検索するクラスを作成
-            NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("UserIDs");
-            Debug.Log("ここまでできた2");
-            //IDの値がID_strと一致するオブジェクトを検索
-            query.WhereEqualTo("ID", ID_str);
-            Debug.Log("ここまでできた3");
-            query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+        //取得テスト
+        //UserIDsを検索するクラスを作成
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("UserIDs");
+        //IDの値がID_strと一致するオブジェクトを検索
+        query.WhereEqualTo("ID", ID_str);
+        query.CountAsync((int count, NCMBException e) =>
+        {
+            if (e != null)
             {
-                Debug.Log("ここまでできた4");
-                if (e != null)
+                //検索失敗時
+                Debug.Log("エラーです");
+            }
+            else
+            {
+                //被っていなければ
+                if (count == 0)
                 {
-                    //検索失敗時
-                    Debug.Log("エラーです");
+                    //IDを表示する
+                    ID_text.text = "ID:" + ID_str;
+
+
+                    //クラスを作成
+                    NCMBObject objClass = new NCMBObject("UserIDs");
+                    // オブジェクトに値を設定
+                    objClass["ID"] = ID_str;
+                    objClass["Name"] = "name";
+                    // データストアへの登録
+                    objClass.SaveAsync();
                 }
+                //被っていたら
                 else
                 {
-                    //検索成功時
-                    foreach (NCMBObject obj in objList)
-                    {
-                        Debug.Log("このデータは存在します");
-                        IDCreateF = true;
-                        Debug.Log("IDF = " + IDCreateF);
-                    }
+                    ID_str = "";
+                    CreateID();
                 }
-            });
-
-            Debug.Log("IDF2 = " + IDCreateF);
-        } while (IDCreateF);
-
-
-        //クラスを作成
-        NCMBObject objClass = new NCMBObject("UserIDs");
-        // オブジェクトに値を設定
-        objClass["ID"] = ID_str;
-        objClass["Name"] = "name";
-        // データストアへの登録
-        objClass.SaveAsync();
+            }
+        });
+        
+        Debug.Log("終了");
     }
 }
