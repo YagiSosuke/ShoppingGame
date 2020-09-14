6413deffe4b3b67617a7fcf034ab8783d9051f23
yagikun <yagikundayo@gmail.com>
2020-09-14 20:44:03 +0900

    リスト作成画面、デザイン

---
+++ ShoppingGame/Assets/Yagi/Scripts/_AnyScene/OpenPanelAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*パネルを展開させたときのアニメーション*/
+
+public class OpenPanelAnim : MonoBehaviour
+{
+    float count = 0;        //時間をカウント
+    public void OpenPanel(GameObject obj)
+    {
+        if (count < 1.0f)
+        {
+            count += Time.deltaTime;
+        }
+        else
+        {
+            count = 1.0f;
+        }
+    }
+}
