ebafd73ab0028ea4caf16c3abc3fdb3900d5be0e
yagikun <yagikundayo@gmail.com>
2020-10-31 22:17:44 +0900

    フレンド一覧シーン - シーンを開いた時リストを上ぞろえした

---
+++ ShoppingGame/Assets/Yagi/Scripts/IDListScene/ScrollAreaSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*IDリストシーン読み込み時、スクロールエリアを上にそろえる*/
+
+public class ScrollAreaSet : MonoBehaviour
+{
+    RectTransform RT;   //幅を調べる変数
+    float Height;       //縦幅
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RT = GetComponent<RectTransform>();
+        Height = RT.rect.y;
+        Debug.Log("Height = " + Height);
+        this.transform.localPosition = new Vector2(0, -Height/2);
+    }
+}
