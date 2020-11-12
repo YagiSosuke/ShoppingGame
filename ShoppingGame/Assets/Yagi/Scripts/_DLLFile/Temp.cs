9361a9873009b7527e969fe02d317bc8617cef3e
yagikun <yagikundayo@gmail.com>
2020-11-12 21:35:42 +0900

    現状保存 (バックアップ)

---
+++ ShoppingGame/Assets/Yagi/Scripts/_DLLFile/Temp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Temp : MonoBehaviour
+{
+    [DllImport("UnityTestDLL")]
+    public static extern int ReturnLog();
+
+    void Start()
+    {
+        Debug.Log("テスト");
+        Debug.Log("cnt = " + ReturnLog());
+    }
+}
