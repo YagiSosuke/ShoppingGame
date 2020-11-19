86d3609827fb00c4500b27b51213f701ad75eeb3
yagikun <yagikundayo@gmail.com>
2020-11-19 22:39:07 +0900

    買い物画面 - バックグラウンドやスリープ中で時間を測れるように

--- ShoppingGame/Assets/Yagi/Scripts/Test.cs
+++ ShoppingGame/Assets/Yagi/Scripts/Test.cs
@@ -5,45 +5,10 @@ using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    private AndroidJavaObject _javaClass = null;
-
     [SerializeField] Text t;
 
     // Start is called before the first frame update
     void Start()
     {
-        _javaClass = new AndroidJavaObject("com.example.mylibrary.NativeCalculaotor");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnClick()
-    {
-        if(_javaClass != null)
-        {
-            float f = _javaClass.Call<float>("Add", 9);
-            t.text = "" + f;
-        }
-    }
-
-    public void aaa()
-    {
-        if (_javaClass != null)
-        {
-            _javaClass.CallStatic("ShowToast");
-        }
-    }
-
-
-
-    private void OnDestroy()
-    {
-        // 解放
-        _javaClass.Dispose();
-        _javaClass = null;
     }
 }
